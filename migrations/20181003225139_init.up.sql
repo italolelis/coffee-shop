c368f1fb68bcba68f68ec48ccfe8d0dd1da44561
Italo Vietro <italo.vietro@n26.com>
2018-10-25 10:39:37 +0200

    cleaning the master branch

--- migrations/20181003225139_init.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "orders" (
-    "id" UUID NOT NULL,
-    "items" JSON NOT NULL,
-    "created_at" TIMESTAMP NOT NULL DEFAULT 'now()',
-    CONSTRAINT "pk_orders" PRIMARY KEY (
-        "id"
-     )
-);
