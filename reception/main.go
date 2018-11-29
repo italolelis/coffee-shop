bf320d04e0608cca16ec480a4c3ea270797f0803
Italo Vietro <italo.vietro@n26.com>
2018-11-29 14:53:38 +0100

    Using basic http metrics
    Signed-off-by: Italo Vietro <italo.vietro@n26.com>

--- reception/main.go
+++ reception/main.go
@@ -8,13 +8,17 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/italolelis/reception/pkg/config"
 	"github.com/italolelis/kit/log"
-	"github.com/italolelis/reception/pkg/reception"
 	"github.com/italolelis/reception/pkg/coffees"
+	"github.com/italolelis/reception/pkg/config"
+	"github.com/italolelis/reception/pkg/reception"
 	"github.com/jmoiron/sqlx"
-	"github.com/rafaeljesus/rabbus"
 	_ "github.com/lib/pq"
+	"github.com/rafaeljesus/rabbus"
+	"go.opencensus.io/exporter/prometheus"
+	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/plugin/ochttp/propagation/b3"
+	"go.opencensus.io/stats/view"
 )
 
 func main() {
@@ -24,7 +28,11 @@ func main() {
 
 	// gets the contextual logging
 	logger := log.WithContext(ctx)
-	defer logger.Sync()
+	defer func() {
+		if err := logger.Sync(); err != nil {
+			logger.Fatal(err)
+		}
+	}()
 
 	// loads the configuration from the enviroment
 	cfg, err := config.Load()
@@ -34,7 +42,7 @@ func main() {
 	log.SetLevel(cfg.LogLevel)
 
 	// setup the event stream. In this case is an event broker because we chose rabbitmq
-	eventStream, err:=setupEventStream(ctx, cfg.EventStream)
+	eventStream, err := setupEventStream(ctx, cfg.EventStream)
 	if err != nil {
 		logger.Fatal(err.Error())
 	}
@@ -55,18 +63,25 @@ func main() {
 		}
 	}()
 
-	go eventStream.Run(ctx)
+	go func() {
+		if err := eventStream.Run(ctx); err != nil {
+			logger.Fatal(err)
+		}
+	}()
 
 	// connects to the primary datastore
 	db, err := sqlx.Connect("postgres", cfg.Database.DSN)
 	if err != nil {
 		logger.Fatal(err.Error())
 	}
-	defer db.Close()
+	defer func() {
+		if err := db.Close(); err != nil {
+			logger.Fatal(err)
+		}
+	}()
 
 	wRepo := reception.NewPostgresWriteRepository(db)
 	rRepo := reception.NewPostgresReadRepository(db)
-
 	coffeeReadRepo := coffees.NewPostgresReadRepository(db)
 
 	// creates the router and register the handlers
@@ -75,13 +90,34 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Timeout(60 * time.Second))
 
+	if err := view.Register(
+		ochttp.ClientSentBytesDistribution,
+		ochttp.ClientReceivedBytesDistribution,
+		ochttp.ClientRoundtripLatencyDistribution,
+	); err != nil {
+		logger.Fatal(err)
+	}
+
+	exporter, err := prometheus.NewExporter(prometheus.Options{
+		Namespace: "coffee-shop",
+	})
+	if err != nil {
+		logger.Fatalw("failed to create the prometheus stats exporter", "err", err.Error())
+	}
+	view.RegisterExporter(exporter)
+	view.SetReportingPeriod(time.Second)
+
+	r.Handle("/metrics", exporter)
 	r.Route("/orders", func(r chi.Router) {
 		r.Post("/", reception.CreateOrderHandler(wRepo, coffeeReadRepo, eventStream))
 		r.Get("/{id}", reception.GetOrderHandler(rRepo))
 	})
 
 	logger.Infow("service running", "port", cfg.Port)
-	logger.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), chi.ServerBaseContext(ctx, r)))
+	logger.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), chi.ServerBaseContext(ctx, &ochttp.Handler{
+		Handler:     r,
+		Propagation: &b3.HTTPFormat{},
+	})))
 }
 
 func setupEventStream(ctx context.Context, cfg config.EventStream) (*rabbus.Rabbus, error) {
