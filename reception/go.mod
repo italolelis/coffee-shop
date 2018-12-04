ac02f457960a44ce87e3c66d593d6aa9c99dbafd
Italo Vietro <italo.vietro@n26.com>
2018-12-05 00:02:21 +0100

    Added open census
    Signed-off-by: Italo Vietro <italo.vietro@n26.com>

--- reception/go.mod
+++ reception/go.mod
@@ -1,21 +1,18 @@
 module github.com/italolelis/reception
 
 require (
-	cloud.google.com/go v0.33.1 // indirect
-	git.apache.org/thrift.git v0.0.0-20181128031924-7f0fa6c21ec2 // indirect
+	contrib.go.opencensus.io/integrations/ocsql v0.1.2
 	github.com/go-chi/chi v3.3.3+incompatible
 	github.com/go-chi/render v1.0.1
 	github.com/go-sql-driver/mysql v1.4.0 // indirect
-	github.com/golang/lint v0.0.0-20181026193005-c67002cb31c3 // indirect
+	github.com/gogo/protobuf v1.1.1 // indirect
 	github.com/golang/protobuf v1.2.0
-	github.com/grpc-ecosystem/grpc-gateway v1.5.1 // indirect
 	github.com/italolelis/kit v0.0.0
 	github.com/jmoiron/sqlx v0.0.0-20180614180643-0dae4fefe7c0
 	github.com/kelseyhightower/envconfig v1.3.0
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/lib/pq v1.0.0
 	github.com/mattn/go-sqlite3 v1.10.0 // indirect
-	github.com/openzipkin/zipkin-go v0.1.3 // indirect
 	github.com/prometheus/client_golang v0.9.1 // indirect
 	github.com/prometheus/common v0.0.0-20181126121408-4724e9255275 // indirect
 	github.com/prometheus/procfs v0.0.0-20181126161756-619930b0b471 // indirect
@@ -25,17 +22,10 @@ require (
 	github.com/sony/gobreaker v0.0.0-20181109014844-d928aaea92e1 // indirect
 	github.com/streadway/amqp v0.0.0-20181107104731-27835f1a64e9 // indirect
 	go.opencensus.io v0.18.0
-	golang.org/x/lint v0.0.0-20181026193005-c67002cb31c3 // indirect
 	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a // indirect
-	golang.org/x/oauth2 v0.0.0-20181120190819-8f65e3013eba // indirect
 	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f // indirect
-	golang.org/x/sys v0.0.0-20181128092732-4ed8d59d0b35 // indirect
-	golang.org/x/tools v0.0.0-20181127232545-e782529d0ddd // indirect
-	google.golang.org/api v0.0.0-20181127174356-0a71a4356c3f // indirect
 	google.golang.org/appengine v1.3.0 // indirect
-	google.golang.org/genproto v0.0.0-20181127195345-31ac5d88444a // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	honnef.co/go/tools v0.0.0-20180920025451-e3ad64cb4ed3 // indirect
 )
 
 replace github.com/italolelis/kit => ../kit
--- reception/main.go
+++ reception/main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"net/http"
 	"time"
 
+	"contrib.go.opencensus.io/integrations/ocsql"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/italolelis/kit/log"
 	"github.com/italolelis/reception/pkg/coffees"
 	"github.com/italolelis/reception/pkg/config"
-	"github.com/italolelis/reception/pkg/reception"
+	"github.com/italolelis/reception/pkg/order"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"github.com/rafaeljesus/rabbus"
+	"go.opencensus.io/exporter/jaeger"
 	"go.opencensus.io/exporter/prometheus"
 	"go.opencensus.io/plugin/ochttp"
 	"go.opencensus.io/plugin/ochttp/propagation/b3"
 	"go.opencensus.io/stats/view"
+	"go.opencensus.io/trace"
 )
 
 func main() {
@@ -34,23 +38,90 @@ func main() {
 		}
 	}()
 
-	// loads the configuration from the enviroment
+	// loads the configuration from the environment
 	cfg, err := config.Load()
 	if err != nil {
 		logger.Fatal(err.Error())
 	}
 	log.SetLevel(cfg.LogLevel)
 
-	// setup the event stream. In this case is an event broker because we chose rabbitmq
-	eventStream, err := setupEventStream(ctx, cfg.EventStream)
+	db, close := setupDatabase(ctx, cfg.Database)
+	defer close()
+
+	metricsHandler := setupMetrics(ctx)
+
+	eventStream, close := setupEventStream(ctx, cfg.EventStream)
+	defer close()
+
+	flush := setupTracing(ctx, cfg.Tracing)
+	defer flush()
+
+	wRepo := order.NewPostgresWriteRepository(db)
+	rRepo := order.NewPostgresReadRepository(db)
+	coffeeReadRepo := coffees.NewPostgresReadRepository(db)
+	orderHandler := order.NewHandler(wRepo, rRepo, coffeeReadRepo, eventStream)
+
+	// creates the router and register the handlers
+	r := chi.NewRouter()
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Handle("/metrics", metricsHandler)
+	r.Route("/orders", func(r chi.Router) {
+		r.Post("/", orderHandler.CreateOrder)
+		r.Get("/{id}", orderHandler.GetOrder)
+	})
+
+	logger.Infow("service running", "port", cfg.Port)
+	logger.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), chi.ServerBaseContext(ctx, &ochttp.Handler{
+		Handler:     r,
+		Propagation: &b3.HTTPFormat{},
+	})))
+}
+
+// setupDatabase connects to the primary data store
+func setupDatabase(ctx context.Context, cfg config.Database) (*sqlx.DB, func()) {
+	logger := log.WithContext(ctx)
+
+	// Register our ocsql wrapper for the provided Postgres driver.
+	driverName, err := ocsql.Register("postgres", ocsql.WithAllTraceOptions())
+	if err != nil {
+		logger.Fatalw("could not register the wrapper provider", "err", err)
+	}
+
+	// Connect to a Postgres database using the ocsql driver wrapper.
+	rawDB, err := sql.Open(driverName, cfg.DSN)
+	if err != nil {
+		logger.Fatalw("could not open a connection with the database", "driver", driverName, "err", err)
+	}
+
+	// Wrap our *sql.DB with sqlx.
+	db := sqlx.NewDb(rawDB, "postgres")
+	return db, func() {
+		if err := db.Close(); err != nil {
+			logger.Fatal(err)
+		}
+	}
+}
+
+// setupEventStream sets up the event stream. In this case is an event broker because we chose rabbitmq
+func setupEventStream(ctx context.Context, cfg config.EventStream) (*rabbus.Rabbus, func()) {
+	logger := log.WithContext(ctx)
+
+	cbStateChangeFunc := func(name, from, to string) {
+		logger.Debugw("rabbitmq state changed", "from", from, "to", to)
+	}
+
+	eventStream, err := rabbus.New(
+		cfg.DSN,
+		rabbus.Durable(true),
+		rabbus.Attempts(cfg.Attempts),
+		rabbus.Sleep(cfg.Backoff),
+		rabbus.Threshold(cfg.Threshold),
+		rabbus.OnStateChange(cbStateChangeFunc),
+	)
 	if err != nil {
 		logger.Fatal(err.Error())
 	}
-	defer func(r *rabbus.Rabbus) {
-		if err := r.Close(); err != nil {
-			logger.Error(err.Error())
-		}
-	}(eventStream)
 
 	go func() {
 		for {
@@ -69,26 +140,37 @@ func main() {
 		}
 	}()
 
-	// connects to the primary datastore
-	db, err := sqlx.Connect("postgres", cfg.Database.DSN)
-	if err != nil {
-		logger.Fatal(err.Error())
-	}
-	defer func() {
-		if err := db.Close(); err != nil {
-			logger.Fatal(err)
+	return eventStream, func() {
+		if err := eventStream.Close(); err != nil {
+			logger.Error(err.Error())
 		}
-	}()
+	}
+}
 
-	wRepo := reception.NewPostgresWriteRepository(db)
-	rRepo := reception.NewPostgresReadRepository(db)
-	coffeeReadRepo := coffees.NewPostgresReadRepository(db)
+// setupTracing Register the Jaeger exporter to be able to retrieve
+// the collected spans.
+func setupTracing(ctx context.Context, cfg config.Tracing) func() {
+	logger := log.WithContext(ctx)
 
-	// creates the router and register the handlers
-	r := chi.NewRouter()
-	r.Use(middleware.RequestID)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Timeout(60 * time.Second))
+	exporter, err := jaeger.NewExporter(jaeger.Options{
+		CollectorEndpoint: cfg.CollectorEndpoint,
+		Process: jaeger.Process{
+			ServiceName: cfg.ServiceName,
+		},
+	})
+	if err != nil {
+		logger.Errorw("could not create the jaeger exporter", "err", err)
+	}
+
+	trace.RegisterExporter(exporter)
+	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+
+	return func() { exporter.Flush() }
+}
+
+// setupMetrics sets up the application metrics
+func setupMetrics(ctx context.Context) http.Handler {
+	logger := log.WithContext(ctx)
 
 	if err := view.Register(
 		ochttp.ClientSentBytesDistribution,
@@ -105,34 +187,7 @@ func main() {
 		logger.Fatalw("failed to create the prometheus stats exporter", "err", err.Error())
 	}
 	view.RegisterExporter(exporter)
-	view.SetReportingPeriod(time.Second)
+	view.SetReportingPeriod(1 * time.Second)
 
-	r.Handle("/metrics", exporter)
-	r.Route("/orders", func(r chi.Router) {
-		r.Post("/", reception.CreateOrderHandler(wRepo, coffeeReadRepo, eventStream))
-		r.Get("/{id}", reception.GetOrderHandler(rRepo))
-	})
-
-	logger.Infow("service running", "port", cfg.Port)
-	logger.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), chi.ServerBaseContext(ctx, &ochttp.Handler{
-		Handler:     r,
-		Propagation: &b3.HTTPFormat{},
-	})))
-}
-
-func setupEventStream(ctx context.Context, cfg config.EventStream) (*rabbus.Rabbus, error) {
-	logger := log.WithContext(ctx)
-
-	cbStateChangeFunc := func(name, from, to string) {
-		logger.Debugw("rabbitmq state changed", "from", from, "to", to)
-	}
-
-	return rabbus.New(
-		cfg.DSN,
-		rabbus.Durable(true),
-		rabbus.Attempts(cfg.Attempts),
-		rabbus.Sleep(cfg.Backoff),
-		rabbus.Threshold(cfg.Threshold),
-		rabbus.OnStateChange(cbStateChangeFunc),
-	)
+	return exporter
 }
